29e14cc3dedcc038f3f08bf557abd6106eb96a28
hassan00dev <muhammadhassan00dev@gmail.com>
2024-12-13 15:03:00 +0500

    get queue entities

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@
 using json = nlohmann::json;
 namespace fs = std::filesystem;
 
-json readJsonFromFile(const std::string& filePath) {
+json readJsonFromFile(const std::string &filePath)
+{
     std::ifstream file(filePath);
 
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         throw std::runtime_error("Could not open the JSON file: " + filePath);
     }
 
@@ -19,17 +21,42 @@ json readJsonFromFile(const std::string& filePath) {
     return jsonData;
 }
 
-int main() {
-    try {
+void displayEntity(const json &entity)
+{
+    std::cout << "Client ID: " << entity["client_id"] << std::endl;
+    std::cout << "Photobooth ID: " << entity["photobooth_id"] << std::endl;
+    std::cout << "Session ID: " << entity["session_id"] << std::endl;
+    std::cout << "Captured Time: " << entity["captured"] << std::endl;
+    std::cout << "Image URL: " << entity["url"] << std::endl;
+    std::cout << "Media Type: " << entity["media_type"] << std::endl;
+    std::cout << "Approval Status: " << entity["approval_status"] << std::endl;
+    std::cout << std::endl;
+}
+
+int main()
+{
+    try
+    {
         fs::path documentsPath = fs::path(std::getenv("HOME")) / "Documents";
 
-        std::string fileName = "photobooth_data.json";
-        std::string jsonFilePath = (documentsPath / fileName).string();
+        std::string settingFilePath = (documentsPath / "photobooth_data.json").string();
+        json settingsData = readJsonFromFile(settingFilePath);
 
-        json jsonData = readJsonFromFile(jsonFilePath);
+        std::string photosPath = settingsData["fileDownloadLocation"];
+        std::cout << "File Download Location: " << photosPath << std::endl;
 
-        std::cout << "JSON Content: " << jsonData.dump(4) << std::endl;
-    } catch (const std::exception& ex) {
+        std::string queuePath = (documentsPath / "upload_queue.json").string();
+        json queueData = readJsonFromFile(queuePath);
+
+        std::cout << "Queue Data: " << queueData << std::endl;
+
+        for (const auto &entry : queueData)
+        {
+            displayEntity(entry);
+        }
+    }
+    catch (const std::exception &ex)
+    {
         std::cerr << "Error: " << ex.what() << std::endl;
     }
 
